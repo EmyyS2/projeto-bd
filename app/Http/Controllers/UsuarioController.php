a2de52839c2062b887e55bdd7e4cfaeee7bff706
EmyyS2 <miacontapcsenai@gmail.com>
2023-08-10 10:45:14 -0300

    .

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use GrahamCampbell\ResultType\Success;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UsuarioController extends Controller
+{
+    public function store(Request $request)
+    {
+        $usuario = Usuario::create([
+            'nome' => $request->nome,
+            'cpf' => $request->cpf,
+            'celular' => $request->celular,
+            'email' => $request->email,
+            'password'=>Hash::make($request->password)
+        ]);
+        return response()->json([
+        "success"=> true,
+        "message"=> "Usuario Cadastrado com sucesso",
+        "data"=> $usuario
+        ]);
+    }
+
+}
