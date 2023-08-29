91ad51a6d336f2c1d00f691a89a383074b830e3d
EmyyS2 <miacontapcsenai@gmail.com>
2023-08-29 15:18:36 -0300

    criando pesquisa

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -17,13 +17,71 @@ class UsuarioController extends Controller
             'cpf' => $request->cpf,
             'celular' => $request->celular,
             'email' => $request->email,
-            'password'=>Hash::make($request->password)
+            'password' => Hash::make($request->password)
         ]);
         return response()->json([
-        "success"=> true,
-        "message"=> "Usuario Cadastrado com sucesso",
-        "data"=> $usuario
+            "success" => true,
+            "message" => "Usuario Cadastrado com sucesso",
+            "data" => $usuario
+        ]);
+    }
+    public function pesquisarPorId($id)
+    {
+        $usuario = Usuario::find($id);
+
+        if ($usuario == null) {
+            return response()->json([
+                'status' => false,
+                'message' => "Usuário não encontrado"
+            ]);
+        }
+
+        return response()->json([
+            'status' => true,
+            'data' => $usuario
+        ]);
+    }
+    public function pesquisarPorCpf($cpf)
+    {
+        $usuario = Usuario::where('cpf', '=', $cpf)->first();
+
+        if ($usuario == null) {
+            return response()->json([
+                'status' => false,
+                'message' => "Usuário não encontrado"
+            ]);
+        }
+
+        return response()->json([
+            'status' => true,
+            'data' => $usuario
         ]);
     }
 
+    public function retornarTodos()
+    {
+        $usuarios = Usuario::all();
+        return response()->json([
+            'status' => true,
+            'data' => $usuarios
+        ]);
+    }
+
+    public function pesquisarPorNome(Request $request)
+    {
+        $usuarios = Usuario::where('nome', 'like', '%' . $request->nome . '%')->get();
+
+        if (count($usuarios) > 0) {
+
+            return response()->json([
+                'status' => true,
+                'data' => $usuarios
+            ]);
+        }
+
+        return response()->json([
+            'status' => false,
+            'message' => 'Não há resultados para pesquisa.'
+        ]);
+    }
 }
