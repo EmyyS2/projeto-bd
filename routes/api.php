74b92a79849702a0742b4611c60879296832abbc
EmyyS2 <miacontapcsenai@gmail.com>
2023-08-29 15:19:00 -0300

    salvando pesquisa

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,17 @@ use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('store', [
-    UsuarioController::class, 'store'
-]);
+Route::post('store',
+ [UsuarioController::class, 'store']);
+
+Route::get('find/{id}',
+[UsuarioController::class, 'pesquisarPorId']);
+
+Route::get('find/cpf/{cpf}',
+[UsuarioController::class, 'pesquisarPorCpf']);
+
+Route::get('all',
+[UsuarioController::class, 'retornarTodos']);
+
+Route::post('nome',
+[UsuarioController::class,'pesquisarPorNome']);
