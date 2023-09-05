44d0ef9e17ae02618c2403846b3f1f5acc2cfd72
EmyyS2 <miacontapcsenai@gmail.com>
2023-09-05 15:13:39 -0300

    atualizações

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -84,4 +84,54 @@ class UsuarioController extends Controller
             'message' => 'Não há resultados para pesquisa.'
         ]);
     }
+
+    public function excluir($id)
+    {
+        $usuario = Usuario::find($id);
+        if (!isset($usuario)) {
+            return response()->json([
+                'status' => false,
+                'message' => "Usuario não encontrado"
+            ]);
+        }
+
+$usuario->delete();
+
+return response()->json([
+    'status'=>true,
+    'message'=> "usuário excluído com sucesso"
+]);
+
+    }
+
+public function update(Request $request){
+$usuario= Usuario::find($request->id);
+if(!isset($usuario)){
+    return response()->json([
+        'status' => false,
+        'message' => "Usuario não encontrado"
+    ]);
+    if(!isset($request->email)){
+        $usuario->email = $request->email;
+    }
+    if(!isset($request->nome)){
+$usuario->nome = $request->nome;
+    }
+
+    if(!isset($request->cpf)){
+$usuario->cpf = $request->cpf;
+    }
+
+
+
+}
+$usuario->update();
+
+return response()->json([
+    'status'=>true,
+    'message'=> "usuário atualizado"
+]);
+
+
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,37 @@ use App\Http\Controllers\UsuarioController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('store',
- [UsuarioController::class, 'store']);
+Route::post(
+    'store',
+    [UsuarioController::class, 'store']
+);
 
-Route::get('find/{id}',
-[UsuarioController::class, 'pesquisarPorId']);
+Route::get(
+    'find/{id}',
+    [UsuarioController::class, 'pesquisarPorId']
+);
 
-Route::get('find/cpf/{cpf}',
-[UsuarioController::class, 'pesquisarPorCpf']);
+Route::get(
+    'find/cpf/{cpf}',
+    [UsuarioController::class, 'pesquisarPorCpf']
+);
 
-Route::get('all',
-[UsuarioController::class, 'retornarTodos']);
+Route::get(
+    'all',
+    [UsuarioController::class, 'retornarTodos']
+);
 
-Route::post('nome',
-[UsuarioController::class,'pesquisarPorNome']);
+Route::post(
+    'nome',
+    [UsuarioController::class, 'pesquisarPorNome']
+);
+
+Route::delete(
+    'delete/{id}',
+    [UsuarioController::class, 'excluir']
+);
+
+Route::put(
+    'update',
+    [UsuarioController::class, 'update']
+);
